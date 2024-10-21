63e00c4a7fd4af9593c81a546e0903e4292331c8
Niketh23 <nikethmdcl2003@gmail.com>
2024-10-21 23:09:02 +0530

    memo file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Memo from './components/Memo'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello world</h1>
+      <Memo/>
     </>
   )
 }
---
+++ src/components/Memo.jsx
@@ -0,0 +1,33 @@
+import { memo, useState } from "react";
+
+const Memo = () => {
+  const [title, setTitle] = useState("My name is niketh");
+  const handleMathOp = () => {
+    setTitle("My name is " + Math.random());
+  };
+  return (
+    <div>
+      <button onClick={handleMathOp}>Click me to change</button>
+      <br />
+      <Header title={title}/>
+      <br />
+      <Header title="my name is raman" />
+      <br />
+      <Header title="my name is raman" />
+      <br />
+      <Header title="my name is raman" />
+    </div>
+  );
+};
+
+// const Header = memo({ title }) => {
+//   return <>{title}</>;
+// };
+
+const Header = (function({title}){
+  return <>
+  {title}
+  </>
+})
+
+export default Memo;
